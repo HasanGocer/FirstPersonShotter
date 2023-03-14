69bb6de17390448f8fbde2e0598adb842571f954
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-03-14 14:58:44 +0300

    v 0.32

--- Assets/Scripts/PlayerMovment/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovment/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Joystick joystick; // Joystick nesnesi
+    Joystick joystick; // Joystick nesnesi
     public float speed = 5f; // Hareket hýzý
     public float rotationSpeed = 100f; // Dönüþ hýzý
     private Rigidbody rb;
@@ -17,42 +17,21 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                joystick.gameObject.SetActive(true);
-                joystick.transform.position = touch.position;
-                joystick.transform.rotation = Quaternion.identity;
-            }
-            if (touch.phase == TouchPhase.Moved)
-            {
-                PointerEventData eventData = new PointerEventData(EventSystem.current);
-                eventData.position = touch.position;
-                joystick.OnPointerDown(eventData);
-            }
-            if (touch.phase == TouchPhase.Ended)
-            {
-                joystick.OnPointerUp(new PointerEventData(EventSystem.current));
-                joystick.gameObject.SetActive(false);
-            }
 
-            // Joystick ile karakterin hareketi
-            float horizontalInput = joystick.Horizontal;
-            float verticalInput = joystick.Vertical;
+        // Joystick ile karakterin hareketi
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
 
-            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-            movement.Normalize();
-            MainManager.Instance.mainPlayer.transform.TransformDirection(movement);
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        movement.Normalize();
+        MainManager.Instance.mainPlayer.transform.TransformDirection(movement);
 
-            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
-            // Joystick ile karakterin dönüþü
-            float rotateInput = joystick.Horizontal;
+        // Joystick ile karakterin dönüþü
+        float rotateInput = joystick.Horizontal;
 
-            Vector3 rotation = new Vector3(0f, rotateInput, 0f);
-            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation * rotationSpeed * Time.fixedDeltaTime));
-        }
+        Vector3 rotation = new Vector3(0f, rotateInput, 0f);
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation * rotationSpeed * Time.fixedDeltaTime));
     }
 }
