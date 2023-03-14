dbb848718aa382cba18bcb049cf7d7b47cffda11
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-03-14 19:37:36 +0300

    v 0.36

--- Assets/Scripts/PlayerMovment/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovment/PlayerMovement.cs
@@ -18,26 +18,25 @@ public class PlayerMovement : MonoSingleton<PlayerMovement>
 
     void FixedUpdate()
     {
-        if (GameManager.Instance.gameStat == GameManager.GameStat.start)
+        if (GameManager.Instance.gameStat == GameManager.GameStat.start && (joystick.Horizontal != 0 || joystick.Vertical != 0))
         {
             // Joystick ile karakterin hareketi
             float horizontalInput = joystick.Horizontal;
             float verticalInput = joystick.Vertical;
 
-            if (horizontalInput != 0 || verticalInput != 0) isUseJoystick = true;
-            else isUseJoystick = false;
-            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-            MainManager.Instance.mainPlayer.transform.TransformDirection(movement);
-            movement.Normalize();
+            isUseJoystick = true;
 
-            rb.MovePosition(rb.transform.localPosition + movement * speed * Time.fixedDeltaTime);
-
-            // Joystick ile karakterin dönüþü
             float rotateInput = joystick.Horizontal;
-
             Vector3 rotation = new Vector3(0, rotateInput, 0);
-            MainManager.Instance.mainPlayer.transform.TransformDirection(rotation);
-            rb.MoveRotation(rb.transform.localRotation * Quaternion.Euler(rotation * rotationSpeed * Time.fixedDeltaTime));
+
+            transform.eulerAngles += rotation * rotationSpeed * Time.fixedDeltaTime;
+
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+            movement = transform.TransformDirection(movement);
+            (movement).Normalize();
+
+            transform.position += movement * speed * Time.fixedDeltaTime;
         }
+        else isUseJoystick = false;
     }
 }
