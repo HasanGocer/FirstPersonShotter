d4046acf67d93c4dc9d02d77de567bc5cbb5bbee
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-03-14 21:30:40 +0300

    v 0.37

---
+++ Assets/Scripts/WallStop.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallStop : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject player = other.gameObject;
+
+            Vector3 back = Vector3.zero;
+
+            if (0 < player.transform.position.x - transform.position.x) back.x = -WallManager.Instance.wallBackDistance;
+            else back.x = WallManager.Instance.wallBackDistance;
+            if (0 < player.transform.position.z - transform.position.z) back.z = -WallManager.Instance.wallBackDistance;
+            else back.z = WallManager.Instance.wallBackDistance;
+
+            player.transform.position -= back;
+        }
+    }
+}
